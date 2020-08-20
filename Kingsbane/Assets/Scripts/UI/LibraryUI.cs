be3438da23b00174449e8e9031f9f1d0629ff503
Joshua <joshuaburton2@gmail.com>
2020-08-20 22:37:06 +1000

    Prototype Updates

---
+++ Kingsbane/Assets/Scripts/UI/LibraryUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LibraryUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tabParents;
+    [SerializeField]
+    private List<GameObject> tabs;
+
+    [SerializeField]
+    private List<GameObject> gridRows;
+
+}
