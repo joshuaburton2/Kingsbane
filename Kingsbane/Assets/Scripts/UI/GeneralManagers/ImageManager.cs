dfe3ac6c1af488c933918878aa400e9dfdb934f3
Joshua <joshuaburton2@gmail.com>
2020-11-19 15:46:46 +1000

    Image Manager

---
+++ Kingsbane/Assets/Scripts/UI/GeneralManagers/ImageManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CategoryEnums;
+
+public class ImageManager : MonoBehaviour
+{
+    [Serializable]
+    private class ImageObject
+    {
+        public string imageName;
+        public Sprite imageSprite;
+    }
+
+    [SerializeField]
+    private Sprite defaultImage;
+    [SerializeField]
+    private List<ImageObject> cardImages;
+    [SerializeField]
+    private List<ImageObject> mapImages;
+}
