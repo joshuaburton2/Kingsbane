9f0c089772f92b1997ca94890b76a18f7b9c17a3
Joshua <joshuaburton2@gmail.com>
2020-12-03 15:57:51 +1000

    Card Selection in Hand

--- Kingsbane/Assets/Scripts/UI/Gameplay/Player Bar/Hand/HandContainer.cs
+++ Kingsbane/Assets/Scripts/UI/Gameplay/Player Bar/Hand/HandContainer.cs
@@ -9,9 +9,19 @@ public class HandContainer : MonoBehaviour, IPointerClickHandler
     float cardSizex;
     [SerializeField]
     float cardSizey;
+    [SerializeField]
+    float ySelectionOffset;
+    [SerializeField]
+    private GameObject buttonArea;
 
+    private bool isSelected;
+
+    private GameObject displayObject;
     private CardDisplay cardDisplay;
     private UpgradeDisplay upgradeDisplay;
+
+    private HandUI handUI;
+    public int handIndex { get; private set; }
 
     private const float defaultScalingFactor = 0.20f;
 
@@ -21,26 +31,55 @@ public class HandContainer : MonoBehaviour, IPointerClickHandler
     /// 
     /// </summary>
     public void InitHandContainer(
+        HandUI _handUI,
+        int _handIndex,
         CardData cardData = null,
         UpgradeData upgradeData = null,
         string containerName = "",
         float scalingFactor = defaultScalingFactor)
     {
+        handUI = _handUI;
+        handIndex = _handIndex;
+        isSelected = false;
+
+        buttonArea.SetActive(false);
+
         transform.parent.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(cardSizex * scalingFactor, cardSizey * scalingFactor);
         //Object is instantiated on the parent of this object in order to prevent conflicts with click handling
         if (cardData == null)
         {
             var newUpgradeObj = GameManager.instance.upgradeManager.CreateUpgrade(upgradeData, gameObject.transform.parent, scaling: scalingFactor);
             newUpgradeObj.name = containerName;
-            cardDisplay = newUpgradeObj.GetComponent<CardDisplay>();
+            //Sibling Index is set to 0 so that the click handler on upgrade display doesn't interfere with the click handler on the container
+            newUpgradeObj.transform.SetSiblingIndex(0);
+            upgradeDisplay = newUpgradeObj.GetComponent<UpgradeDisplay>();
+            displayObject = newUpgradeObj;
         }
-        else if(upgradeData == null)
+        else if (upgradeData == null)
         {
             var newCardObj = GameManager.instance.libraryManager.CreateCardObject(cardData, gameObject.transform.parent, scalingFactor);
             newCardObj.name = containerName;
+            //Sibling Index is set to 0 so that the click handler on card display doesn't interfere with the click handler on the container (which is only used when adding cards to a deck)
+            newCardObj.transform.SetSiblingIndex(0);
             cardDisplay = newCardObj.GetComponent<CardDisplay>();
+            displayObject = newCardObj;
         }
 
+    }
+
+    public void SelectDisplay(bool toSelect)
+    {
+        if (isSelected || toSelect)
+        {
+            isSelected = toSelect;
+            var transformVector = new Vector3(0, (toSelect ? 1 : -1) * ySelectionOffset);
+            displayObject.transform.localPosition += transformVector;
+            buttonArea.SetActive(toSelect);
+
+            if (toSelect)
+                
+                handUI.SelectDisplay(handIndex);
+        }
     }
 
     /// <summary>
@@ -61,7 +100,12 @@ public class HandContainer : MonoBehaviour, IPointerClickHandler
             {
                 cardDisplay.DisplayCardDetail();
             }
-            
+        }
+
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+
+            SelectDisplay(!isSelected);
         }
     }
 }
--- Kingsbane/Assets/Scripts/UI/Gameplay/Player Bar/Hand/HandUI.cs
+++ Kingsbane/Assets/Scripts/UI/Gameplay/Player Bar/Hand/HandUI.cs
@@ -11,6 +11,8 @@ public class HandUI : MonoBehaviour
     [SerializeField]
     private float scalingFactor;
 
+    private List<HandContainer> containerList;
+
     public void DisplayCardList(List<Card> cardList)
     {
 
@@ -18,15 +20,31 @@ public class HandUI : MonoBehaviour
 
     public void DisplayUpgradeList(List<UpgradeData> upgradeList)
     {
+        containerList = new List<HandContainer>();
         GameManager.DestroyAllChildren(handList);
 
+        int index = 0;
         foreach (var upgrade in upgradeList)
         {
             var upgradeContainer = Instantiate(handContainerPrefab, handList.transform);
             upgradeContainer.name = $"Container- {upgrade.Name}";
             var handContainer = upgradeContainer.GetComponentInChildren<HandContainer>();
+            containerList.Add(handContainer);
             var upgradeName = $"Name- {upgrade.Name}";
-            handContainer.InitHandContainer(upgradeData: upgrade, containerName: upgradeName, scalingFactor: scalingFactor);
+            handContainer.InitHandContainer(this, index, upgradeData: upgrade, containerName: upgradeName, scalingFactor: scalingFactor);
+
+            index++;
+        }
+    }
+
+    public void SelectDisplay(int selectedIndex)
+    {
+        foreach (var container in containerList)
+        {
+            if (selectedIndex != container.handIndex)
+            {
+                container.SelectDisplay(false);
+            }
         }
     }
 }
