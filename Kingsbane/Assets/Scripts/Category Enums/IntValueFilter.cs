9a1786279f1a3204b079943520238b5ea1af5b18
Joshua <joshuaburton2@gmail.com>
2021-02-04 22:14:28 +1000

    Filtering Updates

--- Kingsbane/Assets/Scripts/Category Enums/IntValueFilter.cs
+++ Kingsbane/Assets/Scripts/Category Enums/IntValueFilter.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CategoryEnums
 {
@@ -13,9 +15,29 @@ namespace CategoryEnums
 
     public static class IntValueFilterer
     {
-        public static void CheckIntValueFilter(int intValue, Dictionary<IntValueFilter, int> intFilter)
+        public static bool CheckIntValueFilter(int intValue, List<int> valueList, Dictionary<IntValueFilter, int> intFilter)
         {
+            if (intFilter.Count > 1)
+            {
+                var filterOption = intFilter.FirstOrDefault();
 
+                if (filterOption.Key == IntValueFilter.HigherThan)
+                    return intValue >= filterOption.Value;
+
+                else if (filterOption.Key == IntValueFilter.Equal)
+                    return intValue == filterOption.Value;
+
+                else if (filterOption.Key == IntValueFilter.LowerThan)
+                    return intValue <= filterOption.Value;
+
+                else
+                    throw new Exception("Not a valid filter type");
+
+            }
+            else
+            {
+                throw new Exception("Not a valid integer filter");
+            }
         }
     }
 }
