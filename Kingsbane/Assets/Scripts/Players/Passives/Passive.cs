723cf1a654b0b3c1799f7d7e123a7c0f79d5af37
Joshua <joshuaburton2@gmail.com>
2021-04-29 23:02:20 +1000

    Passives

---
+++ Kingsbane/Assets/Scripts/Players/Passives/Passive.cs
@@ -0,0 +1,15 @@
+﻿using CategoryEnums;
+using System.Collections.Generic;
+
+public class Passive
+{
+    public string Name { get; set; }
+    public CardData SourceCard { get; set; }
+    public UpgradeData SourceUpgrade { get; set; }
+    public CardTypes AffectedCardTypes { get; set; }
+    public List<Tags> AffectedCardTags { get; set; }
+    public UnitEnchantment Enchantment { get; set; }
+    public int CostModification { get; set; }
+    public SpecialPassiveEffects SpecialPassive { get; set; }
+
+}
---
+++ Kingsbane/Assets/Scripts/Players/Passives/SpecialPassiveEffects.cs
@@ -0,0 +1,6 @@
+﻿public enum SpecialPassiveEffects
+{
+    LunarEclipse,
+    SolarEclipse,
+    MonsterHunter,
+}
